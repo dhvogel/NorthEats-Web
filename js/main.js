385ff624ff6eff570d0b74876a63a0e544d56262
dhvogel <dhvogel2468@gmail.com>
2016-02-05 10:38:06 -0500

    dashboard work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$( document ).ready(function() {
+$(window).load(function() {
+    $('#first-active').addClass('activeLink');
+    console.log("test");
 
-});
+    $('.active-nav li a').bind('click', function(e) {
+        $('.activeLink').removeClass('activeLink');
+        $(this).addClass('activeLink');
+    })
+})
+
