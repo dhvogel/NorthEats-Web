25f6b59977bd66964fb1eded970edcebe4815a75
dhvogel <dhvogel2468@gmail.com>
2016-02-05 13:36:16 -0500

    user sign up connected to Parse

--- js/main.js
+++ js/main.js
@@ -6,5 +6,45 @@ $(window).load(function() {
         $('.activeLink').removeClass('activeLink');
         $(this).addClass('activeLink');
     })
+
+    Parse.initialize("mU9OJrv5bhGXeIGifLASNHCu5zCubALNwBB7UGHR", "OiOugsAYuAlwCNMXOw3ln5H9B1m7JrSFLDHvQ6XD");
+
+    // var TestObject = Parse.Object.extend("TestObject");
+    // var testObject = new TestObject();
+    //   testObject.save({foo: "bar"}, {
+    //   success: function(object) {
+    //     alert("success!");
+    //   },
+    //   error: function(model, error) {
+    //     alert("error");
+    //   }
+    // });
 })
+
+function signUp() {
+    var email = $("#signup-email").val();
+    var password = $("#signup-password").val();
+    var retypepassword = $("#signup-retypepassword").val();
+    var company = $("#signup-company").val();
+    if (password != retypepassword) {
+        alert("Password and Retyped Password must match.")
+        return
+    }
+    var user = new Parse.User();
+    user.set("email", email);
+    user.set("password", password);
+    user.set("username", company);
+
+
+    user.signUp(null, {
+      success: function(user) {
+        $('#signup').closeModal();
+        window.location.href = "dashboard.html"
+      },
+      error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+}
 
