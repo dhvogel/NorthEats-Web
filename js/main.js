ef3829e7ecea1b69f38b66b3144c64484271202c
dhvogel <dhvogel2468@gmail.com>
2016-02-06 19:19:49 -0500

    dashboard work

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(window).load(function() {
+    Parse.initialize("mU9OJrv5bhGXeIGifLASNHCu5zCubALNwBB7UGHR", "OiOugsAYuAlwCNMXOw3ln5H9B1m7JrSFLDHvQ6XD");
+
+    if (!Parse.User.current() && window.location.pathname.split('/').pop() != "index.html") {
+        window.location.href = "index.html"
+    }
+
     $('#first-active').addClass('activeLink');
-    console.log("test");
 
     $('.active-nav li a').bind('click', function(e) {
         $('.activeLink').removeClass('activeLink');
         $(this).addClass('activeLink');
     })
 
-    Parse.initialize("mU9OJrv5bhGXeIGifLASNHCu5zCubALNwBB7UGHR", "OiOugsAYuAlwCNMXOw3ln5H9B1m7JrSFLDHvQ6XD");
 
     // var TestObject = Parse.Object.extend("TestObject");
     // var testObject = new TestObject();
@@ -51,13 +55,13 @@ function signUp() {
 function logIn() {
     var password = $("#login-password").val();
     var username = $("#login-username").val();
-    if (password != retypepassword) {
-        alert("Password and Retyped Password must match.")
-        return
-    }
+    console.log(password);
+    console.log(username);
+
 
     Parse.User.logIn(username, password, {
       success: function(user) {
+        console.log(user);
         $('#login').closeModal();
         window.location.href = "dashboard.html"
       },
@@ -70,5 +74,25 @@ function logIn() {
 function logOut() {
     Parse.User.logOut();
     window.location.href = "index.html"
+}
+
+function getCurrentOrders() {
+    var CurrentOrders = Parse.Object.extend("Amicis_Orders");
+    var query = new Parse.Query(CurrentOrders);
+    query.equalTo("filled", false);
+    query.descending("createdAt");
+    query.find({
+        success: function(results) {
+           for (var i = 0; i < results.length; i++) { 
+               var object = results[i];
+                   (function($) {
+                       $('#results-table').append('<tr><td>' + object.get('playerName') + '</td><td>' + object.get('score') + '</td></tr>');
+                   })(jQuery);
+           }
+        },
+        error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
 }
 
